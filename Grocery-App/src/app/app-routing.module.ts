b9aa27cd6d1722193ed13514340ec265e94cca94
Dutt Mistry <dutta.mistry@gmail.com>
2023-03-05 22:38:29 +0530

    Grocery-App

--- Grocery-App/src/app/app-routing.module.ts
+++ Grocery-App/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './front/catalogue/category/category.component';
+import { ProductDetailsComponent } from './front/catalogue/product-details/product-details.component';
+import { ProductListComponent } from './front/catalogue/product-list/product-list.component';
+import { LoginComponent } from './front/user/login/login.component';
+import { RegistrationComponent } from './front/user/registration/registration.component';
+import { UserProfileComponent } from './front/user/user-profile/user-profile.component';
 
 
-const routes: Routes = []
+const routes: Routes = [{path:'login',component:LoginComponent},
+{path:'front/user/registration',component:RegistrationComponent},
+{path:'front/user/user-profile',component:UserProfileComponent},
+{path:'front/catalogue/product-details',component:ProductDetailsComponent},
+{path:'front/catalogue/product-list',component:ProductListComponent},
+{path:'front/catalogue/category',component:CategoryComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Grocery-App/src/app/front/front-routing.module.ts
+++ Grocery-App/src/app/front/front-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'login',component:LoginComponent},
+{path:'front/user/registration',component:RegistrationComponent},
+{path:'front/user/user-profile',component:UserProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
