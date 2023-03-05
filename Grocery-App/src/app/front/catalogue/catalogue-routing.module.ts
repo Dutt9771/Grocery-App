2c7bc9a1ed7d4acf24dafe23c773fae7d377eaba
Dutt Mistry <dutta.mistry@gmail.com>
2023-03-05 18:50:09 +0530

    Grocery-App

--- Grocery-App/src/app/front/catalogue/catalogue-routing.module.ts
+++ Grocery-App/src/app/front/catalogue/catalogue-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'front/catalogue/product-details',component:ProductDetailsComponent},
+  {path:'front/catalogue/product-list',component:ProductListComponent},
+  {path:'front/catalogue/category',component:CategoryComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Grocery-App/src/app/front/user/user-routing.module.ts
+++ Grocery-App/src/app/front/user/user-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'front/user/registration',component:RegistrationComponent},
+  {path:'front/user/user-profile',component:UserProfileComponent}
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
