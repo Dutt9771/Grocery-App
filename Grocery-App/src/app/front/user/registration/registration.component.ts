93a20916da4589d45e35811ad1a8a71908f67ef8
Dutt Mistry <dutta.mistry@gmail.com>
2023-03-08 14:50:40 +0530

    Grocery-App

--- Grocery-App/src/app/front/user/registration/registration.component.ts
+++ Grocery-App/src/app/front/user/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup,Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -7,18 +7,173 @@ import { FormControl, FormGroup,Validators } from '@angular/forms';
 })
 export class RegistrationComponent {
 
+  
+  // For State and City
+  selectedState: any;
+  selectedCity: any;
+
+  states = [  
+    {     name: 'Andaman and Nicobar Islands',     cities: ['Port Blair'] 
+  },
+  { 
+    name: 'Andhra Pradesh', 
+    cities: [
+      'Adoni', 'Amaravati', 'Anantapur', 'Chandragiri', 'Chittoor', 
+      'Dowlaiswaram', 'Eluru', 'Guntur', 'Kadapa', 'Kakinada', 
+      'Kurnool', 'Machilipatnam', 'Nagarjunakoṇḍa', 'Rajahmundry', 
+      'Srikakulam', 'Tirupati', 'Vijayawada', 'Visakhapatnam', 
+      'Vizianagaram', 'Yemmiganur'
+    ] 
+  },
+  { 
+    name: 'Arunachal Pradesh', 
+    cities: [
+      'Itanagar'
+    ] 
+  },
+  { 
+    name: 'Assam', 
+    cities: [
+      'Dhuburi', 'Dibrugarh', 'Dispur', 'Guwahati', 'Jorhat', 
+      'Nagaon', 'Sibsagar', 'Silchar', 'Tezpur', 'Tinsukia'
+    ] 
+  },
+  { 
+    name: 'Bihar', 
+    cities: [
+      'Ara', 'Baruni', 'Begusarai', 'Bettiah', 'Bhagalpur', 
+      'Bihar Sharif', 'Bodh Gaya', 'Buxar', 'Chapra', 'Darbhanga', 
+      'Dehri', 'Dinapur Nizamat', 'Gaya', 'Hajipur', 'Jamalpur', 
+      'Katihar', 'Madhubani', 'Motihari', 'Munger', 'Muzaffarpur', 
+      'Patna', 'Purnia', 'Pusa', 'Saharsa', 'Samastipur', 'Sasaram', 
+      'Sitamarhi', 'Siwan'
+    ] 
+  },
+  { 
+    name: 'Chandigarh', 
+    cities: [
+      'Chandigarh'
+    ] 
+  },
+  { 
+    name: 'Chhattisgarh', 
+    cities: [
+      'Ambikapur', 'Bhilai', 'Bilaspur', 'Dhamtari', 'Durg', 
+      'Jagdalpur', 'Raipur', 'Rajnandgaon'
+    ] 
+  },
+  { 
+    name: 'Dadra and Nagar Haveli and Daman and Diu', 
+    cities: [
+      'Daman', 'Diu'
+    ] 
+  },
+  { 
+    name: 'Delhi', 
+    cities: [
+      'Delhi', 'New Delhi'
+    ] 
+  },
+  { 
+    name: 'Goa', 
+    cities: [
+      'Mormugao', 'Panaji'
+    ] 
+  },
+  { 
+    name: 'Gujarat', 
+    cities: [
+      'Ahmedabad', 'Amreli', 'Bharuch', 'Bhavnagar', 'Bhuj', 
+      'Dwarka', 'Gandhinagar', 'Godhra', 'Jamnagar', 'Junagadh', 
+      'Kandla', 'Khambhat', 'Kheda', 'Mahesana', 'Morvi', 'Nadiad', 'Navsari', 'Okha', 'Palanpur',
+      'Patan', 'Porbandar', 'Rajkot', 'Surat', 'Surendranagar',
+      'Valsad', 'Veraval', 'Vadodara'
+      ]
+      },
+      {
+      name: 'Haryana',
+      cities: [
+      'Ambala', 'Bhiwani', 'Chandigarh', 'Faridabad', 'Fatehabad',
+      'Gurgaon', 'Hisar', 'Jhajjar', 'Jind', 'Kaithal', 'Karnal',
+      'Kurukshetra', 'Mahendragarh', 'Narnaul', 'Narwana', 'Palwal',
+      'Panchkula', 'Panipat', 'Rewari', 'Rohtak', 'Sirsa', 'Sonipat',
+      'Tohana', 'Yamunanagar'
+      ]
+      },
+      {
+      name: 'Himachal Pradesh',
+      cities: [
+      'Bilaspur', 'Chamba', 'Dalhousie', 'Dharamsala', 'Hamirpur',
+      'Kangra', 'Kullu', 'Mandi', 'Nahan', 'Shimla', 'Una'
+      ]
+      },
+      {
+      name: 'Jammu and Kashmir',
+      cities: [
+      'Anantnag', 'Baramula', 'Doda', 'Gulmarg', 'Jammu', 'Kathua',
+      'Leh', 'Punch', 'Rajauri', 'Srinagar', 'Udhampur'
+      ]
+      },
+      {
+      name: 'Jharkhand',
+      cities: [
+      'Bokaro', 'Chaibasa', 'Deoghar', 'Dhanbad', 'Dumka', 'Giridih',
+      'Hazaribag', 'Jamshedpur', 'Jharia', 'Rajmahal', 'Ranchi',
+      'Saraikela'
+      ]
+      },
+      {
+      name: 'Karnataka',
+      cities: [
+      'Badami', 'Ballari', 'Bangalore', 'Belgavi', 'Bhadravati',
+      'Bidar', 'Chikkamagaluru', 'Chitradurga', 'Davangere',
+      'Halebidu', 'Hassan', 'Hubballi-Dharwad', 'Kalaburagi',
+      'Kolar', 'Madikeri', 'Mandya', 'Mangaluru', 'Mysuru',
+      'Raichur', 'Shivamogga', 'Shravanabelagola', 'Tumkuru', 'Udupi'
+      ]
+      },
+      {
+      name: 'Kerala',
+      cities: [
+      'Alappuzha', 'Badagara', 'Idukki', 'Kannur', 'Kochi', 'Kollam',
+      'Kottayam', 'Kozhikode', 'Mattancherry', 'Palakkad', 'Thalassery',
+      'Thiruvananthapuram', 'Thrissur'
+      ]
+      },
+      {
+      name: 'Ladakh',
+      cities: [
+      'Kargil', 'Leh'
+      ]
+      }]
+
+  getCities(): string[] {
+    const state = this.states.find(s => s.name === this.selectedState);
+    return state ? state.cities : [];
+  } 
+
+  cities: string[] = [];
+
+// for Validation
+
+
+  // ngOnInit() {
+  //   this.Register = this.fb.group({
+  //     password: ['', [Validators.required]],
+  //     confirm_password: ['', [Validators.required]]
+  //   }, {validator: this.checkPasswords });
+  // }
  @Input() Register :any =new FormGroup({
+   name:new FormControl ("",[
+     Validators.required,
+     Validators.minLength(5)
+   ]),
     email:new FormControl ("",[
       Validators.required,
       Validators.email
     ]),
-    name:new FormControl ("",[
-      Validators.required,
-      Validators.minLength(5)
-    ]),
-    password:new FormControl ("",[
-      Validators.required
-    ]),
+    password: new FormControl('', [Validators.required]),
+    confirm_password: new FormControl ('', [Validators.required]),
     address:new FormControl ("",[
       Validators.required
     ]),
@@ -32,18 +187,17 @@ export class RegistrationComponent {
       Validators.required
     ]),
     zip:new FormControl ("",[
-      Validators.required
+      Validators.required,
+      Validators.minLength(6)
     ]),
 
     })
-
-
 
     get get_Register(){
       return this.Register.controls
     }
     Reg_click(){
-        console.log(this.get_Register.value)
+        console.log(this.Register.value)
   
     }
 }
