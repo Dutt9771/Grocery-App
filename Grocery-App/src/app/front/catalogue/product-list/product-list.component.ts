f6b85036f90a9ae567727766b9f25d3d416a2b0f
Dutt Mistry <dutta.mistry@gmail.com>
2023-03-15 10:43:24 +0530

    Grocery-App

--- Grocery-App/src/app/front/catalogue/product-list/product-list.component.ts
+++ Grocery-App/src/app/front/catalogue/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductListService } from 'src/app/services/product-list.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,59 +10,63 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductListComponent implements OnInit {
  
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,private listservice:ProductListService) {
+    
+  }
 category:any
-  products: any[] = [
-    {
-      name: 'Apple',
-      image: 'https://picsum.photos/id/239/200/300',
-      quantity: 10,
-      price: 20,
-      category: 'fruits'
-    },
-    {
-      name: 'Banana',
-      image: 'https://picsum.photos/id/237/200/300',
-      quantity: 5,
-      price: 10,
-      category: 'fruits'
-    },
-    {
-      name: 'Oranges',
-      image: 'https://picsum.photos/id/236/200/300',
-      quantity: 12,
-      price: 15,
-      category: 'fruits'
-    },
-    {
-      name: 'Carrot',
-      image: 'https://picsum.photos/id/236/200/300',
-      quantity: 12,
-      price: 15,
-      category: 'vegetables'
-    },
-    {
-      name: 'Broccoli',
-      image: 'https://picsum.photos/id/242/200/300',
-      quantity: 7,
-      price: 25,
-      category: 'vegetables'
-    },
-    {
-      name: 'Bread',
-      image: 'https://picsum.photos/id/238/200/300',
-      quantity: 19,
-      price: 30,
-      category: 'bakery'
-    },
-    {
-      name: 'Cake',
-      image: 'https://picsum.photos/id/240/200/300',
-      quantity: 5,
-      price: 50,
-      category: 'bakery'
-    }
-  ];
+  // products: any[] = [
+  //   {
+  //     name: 'Apple',
+  //     image: 'https://picsum.photos/id/239/200/300',
+  //     quantity: 10,
+  //     price: 20,
+  //     category: 'fruits'
+  //   },
+  //   {
+  //     name: 'Banana',
+  //     image: 'https://picsum.photos/id/237/200/300',
+  //     quantity: 5,
+  //     price: 10,
+  //     category: 'fruits'
+  //   },
+  //   {
+  //     name: 'Oranges',
+  //     image: 'https://picsum.photos/id/236/200/300',
+  //     quantity: 12,
+  //     price: 15,
+  //     category: 'fruits'
+  //   },
+  //   {
+  //     name: 'Carrot',
+  //     image: 'https://picsum.photos/id/236/200/300',
+  //     quantity: 12,
+  //     price: 15,
+  //     category: 'vegetables'
+  //   },
+  //   {
+  //     name: 'Broccoli',
+  //     image: 'https://picsum.photos/id/242/200/300',
+  //     quantity: 7,
+  //     price: 25,
+  //     category: 'vegetables'
+  //   },
+  //   {
+  //     name: 'Bread',
+  //     image: 'https://picsum.photos/id/238/200/300',
+  //     quantity: 19,
+  //     price: 30,
+  //     category: 'bakery'
+  //   },
+  //   {
+  //     name: 'Cake',
+  //     image: 'https://picsum.photos/id/240/200/300',
+  //     quantity: 5,
+  //     price: 50,
+  //     category: 'bakery'
+  //   }
+  // ];
+
+
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       // Read category parameter from URL
@@ -77,6 +82,8 @@ category:any
         this.category=category
       }
     });
+    // this.listservice.ProductListShow()
+    // console.log(this.listservice.ProductListShow())
   }
   productArray = [
     {
@@ -230,5 +237,4 @@ category:any
       return 0;
     });
   }
-  
 }
