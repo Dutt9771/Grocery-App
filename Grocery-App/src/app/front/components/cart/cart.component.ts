3008f44a7a2b1ce081142c1e1ccd9dbaf6c79b79
Dutt Mistry <dutta.mistry@gmail.com>
2023-03-23 16:43:17 +0530

    Grocery-App

--- Grocery-App/src/app/front/components/cart/cart.component.ts
+++ Grocery-App/src/app/front/components/cart/cart.component.ts
@@ -12,6 +12,7 @@ cart:any
 // cartItems;
 price:any
 cartObj:any
+
 ngOnInit(){
   this._cartservice.ShowCart().subscribe((res)=>{
     this.cart=res
@@ -25,11 +26,13 @@ ngOnInit(){
   // });
 }
 quantity=1
+Obj:any
 quantitymin(index){
  
   //   console.log(this.cart[index].moneyOfferPrice)
   if(this.cart[index].quantity>1){
    this.cart[index].quantity-=1
+  //  console.log(this.cart)
   // }
   
   }
@@ -38,8 +41,38 @@ quantitymax(index){
  
     // console.log(this.cart[index].moneyOfferPrice)
    this.cart[index].quantity+=1
+  //  console.log(this.cart)
+  // this.ShowCartTotal(index)
+  this.Price=this.cart[index].quantity*this.cart[index].moneyOfferPrice
+//   this.subtotal.push(this.Price)
+// console.log(this.subtotal)
+  // this.Obj={
+  // index:{
+  //   "Price":this.Price
+  // }}
+
+}
+// console.log("this.Obj.index.Price",this.Obj.index.Price)
+GST:any
+Total:any
+Subtotal() {
+  let subtotal:any = 0;
+  for (let i = 0; i < this.cart.length; i++) {
+    subtotal += this.cart[i].quantity * this.cart[i].moneyOfferPrice;
+  }
+  this.GST=subtotal*0.18;
+  this.Total=subtotal+this.GST
+  return subtotal;
+
+}
 
 
+
+Price:any
+totalPrice:any
+ShowCartTotal(){
+  console.log(this.Price)
+  this.totalPrice+=this.price
 }
 
 DelectProduct(id:any){
