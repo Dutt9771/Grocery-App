55d916c6809849298edd288f2611ab46906b8ea5
Dutt Mistry <dutta.mistry@gmail.com>
2023-04-05 23:27:57 +0530

    Update Products

--- Grocery-App/src/app/modules/front/catalogue/products/products.component.ts
+++ Grocery-App/src/app/modules/front/catalogue/products/products.component.ts
@@ -13,130 +13,10 @@ export class ProductsComponent {
 constructor(private _cartservice:CartService,private productservice:ProductsService,private toastr:ToastrService){}
 filteredItems:any=[]
 ngOnInit(){
+  this.GetProducts()
   this.filteredItems=this.productservice.getProducts()
 }
-  productArray:any[] = [
-    {
-      imageurl:"Peach.jpg",
-      category: "Grocery",
-      name: "Organic Brown Rice1",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 8,
-      price:12,
-      money: "USD",
-    },
-    {
-      imageurl:"Peach.jpg",
-      category: "Grocery",
-      name: "Organic Brown Rice2",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 9,
-      price:12,
-      money: "USD",
-    },
-    {
-      imageurl:"Peach.jpg",
-      category: "Grocery",
-      name: "Organic Brown Rice3",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 10,
-      price:12,
-      money: "USD",
-    },
-    {
-      imageurl:"Peach.jpg",
-      category: "Grocery",
-      name: "Organic Brown Rice4",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 10,
-      money: "USD",
-      price:20,
-    },
-    {
-      imageurl:"fruits.jpg",
-      category: "fruits",
-      name: "Fruits",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 2,
-      price:5,
-      money: "USD",
-    },
-    {
-      imageurl:"fruits.jpg",
-      category: "fruits",
-      name: "Fruits",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 3,
-      price:10,
-      money: "USD",
-    },
-    {
-      imageurl:"fruits.jpg",
-      category: "fruits",
-      name: "Fruits",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 3,
-      price:10,
-      money: "USD",
-    },
-    {
-      imageurl:"fruits.jpg",
-      category: "fruits",
-      name: "Fruits",
-      weight: "500 grams",
-      sellerName: "Vishvash Farms Inc.",
-      moneyOfferPrice: 3,
-      price:10,
-      money: "USD",
-    },
-    {
-      imageurl:"Vegetables.jpg",
-      category: "vegetables",
-      name: "Cabbage",
-      weight: "500 grams",
-      sellerName: "Abhay Farms Inc.",
-      moneyOfferPrice: 3,
-      price:10,
-      money: "USD",
-    },
-    {
-      imageurl:"Vegetables.jpg",
-      category: "vegetables",
-      name: "LadiesFinger",
-      weight: "500 grams",
-      sellerName: "Ajay Farms Inc.",
-      moneyOfferPrice: 3,
-      price:10,
-      money: "USD",
-    },
-    {
-      imageurl:"Vegetables.jpg",
-      category: "vegetables",
-      name: "Potato",
-      weight: "500 grams",
-      sellerName: "Organic Farms Inc.",
-      moneyOfferPrice: 3,
-      price:10,
-      money: "USD",
-    },
-    {
-      imageurl:"Vegetables.jpg",
-      category: "vegetables",
-      name: "Tomato",
-      weight: "500 grams",
-      sellerName: "Vishnu Farms Inc.",
-      moneyOfferPrice: 5,
-      price:12,
-      money: "USD",
-    },
-  ]
+  productArray:any[] = []
   ProductAddobj:any;
   clickedItem:any=[]
   ShowcartArr:any=[]
@@ -148,7 +28,15 @@ ngOnInit(){
     console.log("ShowcartArr",this.ShowcartArr)
     return this.ShowcartArr
   }
-
+  GetProducts(){
+    this.productservice.getALLProducts().subscribe({next:(get_all_products_res)=>{
+      console.log("get_all_products_res",get_all_products_res)
+      this.filteredItems=get_all_products_res.data
+      console.log("allProducts",this.filteredItems)
+    },error:(get_all_products_error)=>{
+      console.log("get_all_products_error",get_all_products_error)
+    }})
+  }
 
   Add_cart(i,product){
 
